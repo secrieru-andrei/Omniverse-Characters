893385f4bb49dbd58759404c31b4cbbf184e6b93
Secrieru Andrei <secrieru.andrei@yahoo.com>
2023-03-16 19:17:10 +0200

    Initial commit

---
+++ TaskApp/TaskApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TaskApp
+//
+//  Created by Secrieru Andrei on 16.03.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()§
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TaskApp/TaskApp/TaskAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskAppApp.swift
+//  TaskApp
+//
+//  Created by Secrieru Andrei on 16.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct TaskAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
